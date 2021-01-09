b1174eab7935d45d7491d54268ae278485bb1009
unknown <you@example.com>
2021-01-09 12:07:35 +0100

    tdsimaven

---
+++ src/main/java/com/testProjet/tdsi/App.java
@@ -0,0 +1,13 @@
+package com.testProjet.tdsi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
